a7541e513c7cf21b0d5763dccfc81bee04489e60
asiyanbi mubashir <asiyanbimubashir@gmail.com>
2022-02-10 10:34:53 +0100

    feat: emojis and tranformation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, Pressable} from 'react-native';
+import {emojis} from './data';
 
 const App = () => {
+  const [activeEmoji, setActiveEmoji] = useState(-1);
+  const handleAnimation = (emojiIndex: number) => {
+    setActiveEmoji(emojiIndex);
+  };
   return (
     <View style={styles.container}>
-      {Array(5)
-        .fill('😀')
-        .map(icon => (
-          <Text style={styles.emoji}>{icon}</Text>
+      <View style={styles.emojisWrapper}>
+        {emojis.map(({emoji, emojiName}, emojiIndex) => (
+          <Pressable
+            key={emojiIndex}
+            onPress={() => handleAnimation(emojiIndex)}>
+            <View style={[styles.emojiWrapper]}>
+              <Text
+                style={[
+                  styles.emoji,
+                  activeEmoji === emojiIndex && styles.transformEmoji,
+                ]}>
+                {emoji}
+              </Text>
+              <Text
+                style={[
+                  styles.emojiName,
+                  activeEmoji === emojiIndex && styles.transformEmojiText,
+                ]}>
+                {emojiName}
+              </Text>
+            </View>
+          </Pressable>
         ))}
+      </View>
     </View>
   );
 };
@@ -16,12 +40,30 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  emojisWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '87%',
+  },
+  emojiWrapper: {
+    alignItems: 'center',
+  },
   emoji: {
-    color: 'red',
+    fontSize: 30,
+  },
+  transformEmoji: {
+    transform: [{scale: 1.5}],
+  },
+  transformEmojiText: {
+    transform: [{scale: 1.2}],
+  },
+  emojiName: {
+    color: 'rgba(128,128,128,0.5)',
+    marginTop: 7,
   },
 });
 
---
+++ data/index.ts
@@ -0,0 +1,8 @@
+export const emojis = [
+  {emoji: '👍', emojiName: 'Like'},
+  {emoji: '💗', emojiName: 'Love'},
+  {emoji: '🤣', emojiName: 'Haha'},
+  {emoji: '😲', emojiName: 'Wow'},
+  {emoji: '😥', emojiName: 'Sad'},
+  {emoji: '😞', emojiName: 'Angry'},
+];
